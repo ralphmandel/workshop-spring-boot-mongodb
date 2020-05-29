6b1003624de7ae28ac107253b367f07f639bfa11
ralphmandel <ralphmandelbarros@gmail.com>
2020-05-29 17:39:54 -0300

    Projeto criado

---
+++ src/test/java/com/ralphmandel/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ralphmandel.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
