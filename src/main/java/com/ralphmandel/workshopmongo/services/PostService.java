311b0f3e1ecf24a639918f8ab76f0df2810b80c5
ralphmandel <ralphmandelbarros@gmail.com>
2020-05-30 20:25:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ralphmandel/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.ralphmandel.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ralphmandel.workshopmongo.domain.Post;
+import com.ralphmandel.workshopmongo.repository.PostRepository;
+import com.ralphmandel.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
